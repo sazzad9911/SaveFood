f2fd32967e5f6a6695c8b09958874116792ba0d6
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-01-12 05:36:12 +0600

    Cart update

--- components/User/UserHome.js
+++ components/User/UserHome.js
@@ -4,16 +4,39 @@ import Cart from './../cart/Cart'
 import model from './../Styles/model';
 
 const UserHome = () => {
+    const [data,setData] =React.useState([
+        {
+            name: 'Sazzad Hossain',
+            address: 'Ashulia, Saver, Dhaka',
+            id: 1
+        },
+        {
+            name: 'Sayid Hasan',
+            address: 'Ashulia, Saver, Dhaka',
+            id: 2
+        },
+        {
+            name: 'Nahiduzzaman',
+            address: 'Ashulia, Saver, Dhaka',
+            id: 3
+        },
+        {
+            name: 'Akash',
+            address: 'Ashulia, Saver, Dhaka',
+            id: 4
+        }
+    ])
     return (
         <ScrollView>
             <View style={{
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
-                <Cart />
-                <Cart />
-                <Cart />
-                <Cart />
+                {
+                    data.map((data)=>(
+                        <Cart key={data.id} name={data.name} address={data.address}/>
+                    ))
+                }
             </View>
         </ScrollView>
     );
